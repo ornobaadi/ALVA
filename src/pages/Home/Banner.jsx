4a3b8189b71be8393750296e081f6d2156d91461
ornobaadi <aadi4789@gmail.com>
2025-02-10 22:24:46 +0600

    Banner

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,16 +1,23 @@
+import { FaPlayCircle } from "react-icons/fa";
 
 const Banner = () => {
     return (
         <div>
-            <div className="hero bg-base-200 min-h-screen inter-tight">
-                <div className="hero-content text-center">
-                    <div className="">
-                        <h1 className="text-7xl font-bold">Hello there</h1>
-                        <p className="text-3xl py-6">
-                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                            quasi. In deleniti eaque aut repudiandae et a id nisi.
+            <div className="inter-tight hero bg-neutral text-white min-h-screen">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                        className="max-w-sm rounded-lg shadow-2xl" />
+                    <div>
+                        <h1 className="text-5xl font-bold">Build Your Digital Future</h1>
+                        <p className="py-6 max-w-2xl text-gray-400">
+                            At ALVA, we create reliable web and software solutions tailored to your needs. From frontend to full-stack, we bring your ideas to life.
                         </p>
-                        <button className="btn btn-primary">Get Started</button>
+                        <div className="flex gap-5">
+                            <button className="btn btn-accent">Get Started</button>
+                            <button className="btn btn-accent btn-outline">
+                                <FaPlayCircle /> Watch Demo</button>
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ src/pages/Home/Services.jsx
@@ -0,0 +1,21 @@
+
+const Services = () => {
+    return (
+        <div>
+            <div className="hero bg-base-100">
+                <div className="hero-content flex-col lg:flex-row">
+                    <div>
+                        <h1 className="text-5xl font-bold">Box Office News!</h1>
+                        <p className="py-6">
+                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                            quasi. In deleniti eaque aut repudiandae et a id nisi.
+                        </p>
+                    </div>
+                        <button className="btn btn-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
